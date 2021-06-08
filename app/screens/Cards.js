e78cfc2dea2f2a1cb4695c2fd966fd69a6e0068e
Jacqueline de Leeuw <jacqueline.de.leeuw@gmail.com>
2021-06-08 18:05:58 +0100

    auto swipe

--- app/screens/Cards.js
+++ app/screens/Cards.js
@@ -43,12 +43,18 @@ export default function Cards({ card, handleBack, handleLeftSwipe, handleRightSw
       setOptionThree(['#00A0FF', '#80DED0'])
       setOptionFour(['#00A0FF', '#80DED0'])
       setOption(['#4ca783', '#63dc99'])
+      setTimeout(() => {
+        handleRightSwipe()
+      }, 1000)
     } if (answer !== correct) {
       setOptionOne(['#00A0FF', '#80DED0'])
       setOptionTwo(['#00A0FF', '#80DED0'])
       setOptionThree(['#00A0FF', '#80DED0'])
       setOptionFour(['#00A0FF', '#80DED0'])
       setOption(['#e94a6e', '#f77948'])
+      setTimeout(() => {
+        handleLeftSwipe()
+      }, 1000)
     }
   }
 
